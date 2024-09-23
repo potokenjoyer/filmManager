b5376b5404c520f1616a5efdea75f9a87b167a52
potokenjoyer <potokenjoyer@gmail.com>
2024-09-23 14:35:59 +0300

    fixed input values

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="login-page">
     <div class="form">
-      <h1 class="logo">fWatch</h1>
-      <form class="login-form">
-        <!-- Login -->
-        <el-input
-          placeholder="Введите логин"
-          class="input-login"
-          v-model="inputLogin"
-        />
-        <!-- Password -->
-        <el-input
-          v-model="inputPass"
-          class="input-password"
-          type="password"
-          placeholder="Введите пароль"
-          show-password
-        />
-        <el-button type="primary" class="btn-enter">Вход</el-button>
-      </form>
-      <div class="links-container">
-        <div><el-link type="primary">Забыли пароль?</el-link></div>
-        <div><el-link type="primary">Зарегистрироваться</el-link></div>
-      </div>
+    <h1 class="logo">fWatch</h1>
+    <form class="login-form">
+      <!-- Login -->
+      <el-input
+        placeholder="Введите логин"
+        class="input-login"
+        v-model="inputLogin"
+      />
+      <!-- Password -->
+      <el-input
+        v-model="inputPass"
+        class="input-password"
+        type="password"
+        placeholder="Введите пароль"
+        show-password
+      />
+      <el-button type="primary" class="btn-enter">Вход</el-button>
+    </form>
+    <div class="links-container">
+      <div><el-link type="primary">Забыли пароль?</el-link></div>
+      <div><el-link type="primary">Зарегистрироваться</el-link></div>
     </div>
+  </div>
   </div>
 </template>
 
-<script>
+<script lang="ts" setup>
 import { ref } from "vue";
 const inputPass = ref("");
 const inputLogin = ref("");
@@ -65,6 +65,8 @@ const inputLogin = ref("");
   padding-bottom: 10px;
   text-align: center;
   border-radius: 10px;
+  box-shadow: 100px black;
+  
 }
 
 .input-login {
